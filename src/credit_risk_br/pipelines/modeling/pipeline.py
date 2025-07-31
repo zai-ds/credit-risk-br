049f6379216911507bd0aff657fce14a03557d9b
zai-ds <53749701+zai-ds@users.noreply.github.com>
2025-07-31 13:33:04 -0300

    Create src/credit_risk_br/pipelines/modeling/pipeline.py

---
+++ src/credit_risk_br/pipelines/modeling/pipeline.py
@@ -0,0 +1,24 @@
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import split_data, train_model, evaluate_model
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
+        node(
+            func=split_data,
+            inputs=dict(data="model_input_table", target_column="params:target_column"),
+            outputs=["X_train", "X_test", "y_train", "y_test"],
+            name="split_data_node"
+        ),
+        node(
+            func=train_model,
+            inputs=["X_train", "y_train"],
+            outputs="model",
+            name="train_model_node"
+        ),
+        node(
+            func=evaluate_model,
+            inputs=["model", "X_test", "y_test"],
+            outputs="model_auc",
+            name="evaluate_model_node"
+        ),
+    ])
