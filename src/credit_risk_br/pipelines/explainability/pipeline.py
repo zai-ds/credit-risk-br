b42307ffd2b99df24d1409d7286da49841c3f857
zai-ds <53749701+zai-ds@users.noreply.github.com>
2025-08-01 10:11:15 -0300

    Update explainability/pipeline.py

--- src/credit_risk_br/pipelines/explainability/pipeline.py
+++ src/credit_risk_br/pipelines/explainability/pipeline.py
@@ -1,5 +1,5 @@
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import calculate_shap_values
+from .nodes import calculate_shap_values, generate_shap_summary_plot
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
@@ -8,5 +8,11 @@ def create_pipeline(**kwargs) -> Pipeline:
             inputs=["model", "X_test"],
             outputs="shap_values",
             name="calculate_shap_values_node"
-        )
+        ),
+        node(
+            func=generate_shap_summary_plot,
+            inputs=["shap_values", "X_test", "params:shap_plot_path"],
+            outputs=None,
+            name="generate_shap_summary_plot_node"
+        ),
     ])
