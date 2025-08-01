b39b41b58545bbb6d33fdc89d0ef1f0175c7fa6c
zai-ds <53749701+zai-ds@users.noreply.github.com>
2025-07-31 21:43:17 -0300

    Create pipeline.py

---
+++ src/credit_risk_br/pipelines/data_ingestion/pipeline.py
@@ -0,0 +1,15 @@
+from kedro.pipeline import Pipeline, node, pipeline
+from credit_risk_br.pipelines.data_ingestion import nodes
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=nodes.ingest_data,
+                inputs=None,
+                outputs="raw_dataset",
+                name="ingest_data_node",
+            ),
+        ]
+    )
