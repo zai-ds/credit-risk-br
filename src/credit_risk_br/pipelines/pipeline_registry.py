7ce1ce3250d2f1c646b101d6014ec3c4da177c92
zai-ds <53749701+zai-ds@users.noreply.github.com>
2025-07-31 22:01:29 -0300

    Update pipeline_registry.py

--- src/credit_risk_br/pipelines/pipeline_registry.py
+++ src/credit_risk_br/pipelines/pipeline_registry.py
@@ -1,14 +1,17 @@
-from kedro.framework.project import find_pipelines
+from typing import Dict
 from kedro.pipeline import Pipeline
+from credit_risk_br.pipelines import data_ingestion as di
 from credit_risk_br.pipelines import data_engineering as de
 from credit_risk_br.pipelines import modeling as md
 
 def register_pipelines() -> Dict[str, Pipeline]:
+    data_ingestion_pipeline = di.create_pipeline()
     data_engineering_pipeline = de.create_pipeline()
     modeling_pipeline = md.create_pipeline()
 
     return {
+        "di": data_ingestion_pipeline,
         "de": data_engineering_pipeline,
         "modeling": modeling_pipeline,
-        "__default__": data_engineering_pipeline + modeling_pipeline,
+        "__default__": data_ingestion_pipeline + data_engineering_pipeline + modeling_pipeline,
     }
